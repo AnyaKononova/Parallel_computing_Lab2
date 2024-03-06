d9abd9f4a4201f9c9e35d98aac4101af6dd9db86
AnyaKononova <82609324+AnyaKononova@users.noreply.github.com>
2024-03-06 21:42:20 +0300

    Add files via upload

---
+++ Lab22.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+#include <chrono>
+
+std::string sharedString = "x";
+std::mutex wait;
+
+void axWorker() {
+
+    while (sharedString.size() < 30) {
+        wait.lock();
+        sharedString += "a";
+        std::this_thread::sleep_for(std::chrono::nanoseconds(500000000));
+        sharedString += "x";
+        wait.unlock();
+        std::cout << " " << sharedString << std::endl;
+    }
+
+}
+
+int main() {
+
+    std::thread aThread(axWorker);
+    std::thread xThread(axWorker);
+    aThread.join();
+    xThread.join();
+
+    std::cout << "Final string: " << sharedString << std::endl;
+
+    return 0;
+}
